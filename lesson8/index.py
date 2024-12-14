18a18d8c705533b7ef0677c7b3bd0f60bc467d24
Student <lekeberisha10@mail.com>
2024-12-14 02:54:38 -0800

    ...

---
+++ lesson8/index.py
@@ -0,0 +1,16 @@
+try:
+    result = 10/10
+    print(f"the result of {10} divided by {10} is: {result})
+
+    fruits {
+        "apple":5
+        "bananna":6
+        "orange":8
+    }
+
+    try:
+        print=(pinapple);
+
+        exept error{
+        print("")
+        }
